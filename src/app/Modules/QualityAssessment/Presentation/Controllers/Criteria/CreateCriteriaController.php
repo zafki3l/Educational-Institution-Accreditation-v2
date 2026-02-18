2e431fb03641b676bdca1df9dfee5d5bfd41ac50
Th41 <phamdinhthai2005@gmail.com>
2026-02-18 13:28:48 +0000

    delete create criteria route

--- src/app/Modules/QualityAssessment/Presentation/Controllers/Criteria/CreateCriteriaController.php
+++ src/app/Modules/QualityAssessment/Presentation/Controllers/Criteria/CreateCriteriaController.php
@@ -5,33 +5,15 @@ namespace App\Modules\QualityAssessment\Presentation\Controllers\Criteria;
 use App\Modules\QualityAssessment\Application\UseCases\Criteria\CreateCriteriaUseCase;
 use App\Modules\QualityAssessment\Presentation\Controllers\QualityAssessmentController;
 use App\Modules\QualityAssessment\Presentation\Requests\Criteria\CreateCriteriaRequest;
-use App\Shared\Application\Contracts\StandardReader\StandardReaderInterface;
 use App\Shared\Exception\DomainException;
 use App\Shared\Response\JsonResponse;
-use App\Shared\Response\ViewResponse;
 use App\Shared\SessionManager\AuthSession;
 
 final class CreateCriteriaController extends QualityAssessmentController
 {
     public function __construct(
-        private StandardReaderInterface $standardReader,
         private CreateCriteriaUseCase $createCriteriaUseCase
     ) {}
-
-    public function create(): ViewResponse
-    {
-        $standards = $this->standardReader->all();
-
-        return new ViewResponse(
-            self::MODULE_NAME,
-            'criteria/create',
-            'main.layouts',
-            [
-                'title' => 'create',
-                'standards' => $standards
-            ]
-        );
-    }
 
     public function store(CreateCriteriaRequest $request): JsonResponse
     {
