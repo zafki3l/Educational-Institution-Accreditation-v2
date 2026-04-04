b05fe5206594f3a087e106c6d4674a354fbfc611
Th41 <phamdinhthai2005@gmail.com>
2026-04-04 17:02:09 +0000

    update

--- src/app/Modules/QualityAssessment/Domain/Services/EvidencePermissionCheckerInterface.php
+++ src/app/Modules/QualityAssessment/Domain/Services/EvidencePermissionCheckerInterface.php
@@ -4,5 +4,5 @@ namespace App\Modules\QualityAssessment\Domain\Services;
 
 interface EvidencePermissionCheckerInterface
 {
-    public function check(string $criteria_id, string $actor_id): void;
+    public function check(string $criteria_id, string $actor_id): bool;
 }
